dc6addad51b3d37950ee7932363e4986c728a645
강현준 <hyunjune94@naver.com>
2022-05-16 16:55:24 +0900

    c

--- src/main/java/com/projectfw/infra/HomeController.java
+++ src/main/java/com/projectfw/infra/HomeController.java
@@ -1,7 +1,12 @@
 package com.projectfw.infra;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -10,6 +15,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projectfw.infra.modules.member.Member;
+
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +45,81 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+		@RequestMapping(value = "/test/memberView")
+		public String memberView(Model model) throws Exception{
+			
+			   //api 를 호출해서 데이터를 받아온다.
+			
+			   String apiUrl = "http://localhost:8080/infra/rest/member/3";
+			   
+			   URL url = new URL(apiUrl);
+			   HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			   httpURLConnection.setRequestMethod("GET");
+			   
+			   BufferedReader bufferedReader;
+			   if (httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <= 300) {
+				   bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+			   } else {
+				   bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+			   }
+			   
+			   StringBuilder stringBuilder = new StringBuilder();
+			   String line;
+			   while  ((line = bufferedReader.readLine()) != null) {
+					/* System.out.println("line: " + line); */
+				   stringBuilder.append(line);
+			   }
+			   
+			   bufferedReader.close();
+			   httpURLConnection.disconnect();
+			   
+			   //-------------------------			  
+			   
+			   ObjectMapper objectMapper = new ObjectMapper();
+			   Member member = objectMapper.readValue(stringBuilder.toString(), Member.class);
+			   
+			   model.addAttribute("item", member);
+			   
+			return "test/memberView";
+	}
+		
+		@RequestMapping(value = "/test/memberList")
+		public String memberList(Model model) throws Exception{
+			
+			//api 를 호출해서 데이터를 받아온다.
+			
+			String apiUrl = "http://localhost:8080/infra/rest/member";
+			
+			URL url = new URL(apiUrl);
+			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			httpURLConnection.setRequestMethod("GET");
+			
+			BufferedReader bufferedReader;
+			if (httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <= 300) {
+				bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+			} else {
+				bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+			}
+			
+			StringBuilder stringBuilder = new StringBuilder();
+			String line;
+			while  ((line = bufferedReader.readLine()) != null) {
+				/* System.out.println("line: " + line); */
+				stringBuilder.append(line);
+			}
+			
+			bufferedReader.close();
+			httpURLConnection.disconnect();
+			
+			//-------------------------
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			List<Member> memberList = objectMapper.readValue(stringBuilder.toString(), new TypeReference<List<Member>>() {});
+			
+			model.addAttribute("list", memberList);
+			
+			return "test/memberList";
+		}
 	
 }
